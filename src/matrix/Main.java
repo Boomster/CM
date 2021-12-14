35f53de36b9ad72a3ba2a0dc0b65c4357ccd0cda
Boomster <b.oleinikov@seuslab.ru>
2021-12-14 23:58:42 +0500

    Only numbers left

--- src/matrix/Main.java
+++ src/matrix/Main.java
@@ -1,23 +1,242 @@
 package matrix;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     static List<List<Double>> GetMatrix(){
-        File myFile =  new File("input.txt");
-        return null;
+        try {
+            File myFile = new File("input.txt");
+            Scanner myScanner = new Scanner(myFile);
+            List<List<Double>> field = new ArrayList<>();
+            while(myScanner.hasNextLine()){
+                String sLine = myScanner.nextLine();
+                List<Double> line = new ArrayList<>();
+                String[] wordLine = sLine.split(" ");
+                for (String s :
+                     wordLine) {
+                    line.add(Double.parseDouble(s));
+                }
+                field.add(line);
+            }
+            return field;
+        } catch (Exception e) {
+            System.out.println("Error in input from file!");
+            return null;
+        }
     }
-    static void MatrixIterations(MyMatrix matrix){
-        for(int i = 0; i < matrix.field.size(); i++){
 
+    static void MatrixIterations(MyMatrix taskMatrix){
+        String fileName = "output.txt";
+        List<List<Double>> rootXArray = new ArrayList<>();
+
+        for (int i = 0; i < taskMatrix.field.size(); i++) {
+            List<Double>  rootLine = new ArrayList<>();
+            rootLine.add(i+1.);
+            rootXArray.add(rootLine);
         }
 
+        MyMatrix rootX = new MyMatrix(rootXArray);
+        PrintLine(rootX.ToLine(), fileName);
+        PrintMatrix(taskMatrix, fileName);
+        MyMatrix b = MyMatrix.Mult(taskMatrix, rootX);
+        PrintLine(b.ToLine(), fileName);
+
+
+        MyMatrix U = MyMatrix.Copy(taskMatrix);
+        MyMatrix L = new MyMatrix(taskMatrix.field.size());
+
+        List<Integer> lineChange = new ArrayList<>();//P
+        for (int i = 0; i < U.field.size(); i++) {
+            lineChange.add(i);
+        }
+
+        for(int i = 0; i < U.field.size(); i++){
+            Double maxElem = U.field.get(i).get(i);
+            int maxNum = i;
+            for(int j = i + 1; j < U.field.size(); j++){
+                if(Math.abs(maxElem) < Math.abs(U.field.get(j).get(i))){
+                    maxElem = U.field.get(j).get(i);
+                    maxNum = j;
+                }
+            }
+            U.SwapLines(i, maxNum);
+            int tmp = lineChange.get(i);
+            lineChange.set(i, lineChange.get(maxNum));
+            lineChange.set(maxNum, tmp);
+
+            for (int j = 0; j < U.field.size(); j++) {
+                U.field.get(i).set(j, U.field.get(i).get(j)/maxElem);
+            }
+            for (int j = i + 1; j < U.field.size(); j++) {
+                Double tmpFirst = U.field.get(j).get(i);
+                for (int k = i; k < U.field.size(); k++) {
+                    U.field.get(j).set(k, U.field.get(j).get(k) - U.field.get(i).get(k)*tmpFirst);
+                }
+            }
+            PrintLine(lineChange.toString(), fileName);
+            PrintLine("U:", fileName);
+            PrintMatrix(U, fileName);
+
+            if(i > 0) {
+                //U^-1
+                MyMatrix tmpU = new MyMatrix();
+                for (int j = 0; j < i; j++) {
+                    List<Double> line = new ArrayList<>();
+                    for (int k = 0; k < i; k++) {
+                        line.add(U.field.get(j).get(k));
+                    }
+                    tmpU.field.add(line);
+                }
+                MyMatrix smallMinusU = tmpU.MinusOnePower();
+
+                //d^T
+                MyMatrix d = new MyMatrix();
+                d.field.add(new ArrayList<>());
+                for (int j = 0; j < i; j++) {
+                    d.field.get(0).add(taskMatrix.field.get(lineChange.get(i)).get(j));
+                }
+
+                //l^T(k-1)
+                MyMatrix lMinus = MyMatrix.Mult(d, smallMinusU);
+
+                //u(k-1)
+                MyMatrix u = new MyMatrix();
+                for (int j = 0; j < i; j++) {
+                    List<Double> line = new ArrayList<>();
+                    line.add(U.field.get(j).get(i));
+                    u.field.add(line);
+                }
+                //tmp lxu
+                Double lxu = (MyMatrix.Mult(lMinus, u)).field.get(0).get(0);
+                //l (k,k)
+                Double lkk = taskMatrix.field.get(lineChange.get(i)).get(i) - lxu;
+                for (int j = 0; j < i; j++) {
+                    L.field.get(i).set(j, lMinus.field.get(0).get(j));
+                }
+                L.field.get(i).set(i, lkk);
+            } else {
+                L.field.get(0).set(0, taskMatrix.field.get(lineChange.get(0)).get(0));
+            }
+            PrintLine("L:", fileName);
+            PrintMatrix(L, fileName);
+        }
+
+        //LU
+        MyMatrix LU = MyMatrix.Mult(L, U);
+        MyMatrix LUminusPA = new MyMatrix(taskMatrix.field.size());//LU-PA
+        for (int i = 0; i < taskMatrix.field.size(); i++) {
+            for (int j = 0; j < taskMatrix.field.size(); j++) {
+                LUminusPA.field.get(i).set(j, LU.field.get(i).get(j)-taskMatrix.field.get(lineChange.get(i)).get(j));
+            }
+        }
+        PrintLine("LU - PA", fileName);
+        PrintMatrixPrecision(LUminusPA, fileName);
+        PrintLine("Determinant A", fileName);
+        PrintLine(String.format("%.5f",MyMatrix.det(taskMatrix)), fileName);
+
+        //get x from LU
+        MyMatrix vb = new MyMatrix();
+        for (int i = 0; i < b.field.size(); i++) {
+            List<Double> line = new ArrayList<>();
+            line.add(b.field.get(lineChange.get(i)).get(0));
+            vb.field.add(line);
+        }
+
+        MyMatrix y = MyMatrix.Mult(L.MinusOnePower(), vb);
+        MyMatrix x = new MyMatrix();
+        Double[] tmpX = new Double[U.field.size()];
+        for (int i = U.field.size() -1; i >= 0; i--) {
+            tmpX[i] = y.field.get(i).get(0);
+            for (int j = U.field.size()-1; j > i; j--) {
+                tmpX[i] -= tmpX[j]*U.field.get(i).get(j);
+            }
+
+        }
+        x.field.add(new ArrayList<>());
+        for (int i = 0; i < U.field.size(); i++) {
+            x.field.get(0).add(tmpX[i]);
+        }
+
+        PrintLine("x", fileName);
+        PrintLine(x.ToLine(), fileName);
+
+        MyMatrix newA = new MyMatrix();
+        for (int i = 0; i < LU.field.size(); i++) {
+            List<Double> line = new ArrayList<>();
+            for (int j = 0; j < LU.field.size(); j++) {
+                line.add(LU.field.get(lineChange.indexOf(i)).get(j));
+            }
+            newA.field.add(line);
+        }
+        PrintLine("A", fileName);
+        PrintMatrix(newA, fileName);
+        PrintLine("A^-1", fileName);
+        MyMatrix minusA = newA.MinusOnePower();
+        PrintMatrix(minusA, fileName);
+        PrintLine("A*A^-1", fileName);
+        PrintMatrixPrecision(MyMatrix.Mult(newA, minusA), fileName);
+
     }
+
+    public static void PrintMatrixPrecision(MyMatrix matrix, String fileName){
+        try{
+            FileWriter myWriter = new FileWriter(fileName, true);
+            List<String> matrixLines = matrix.ToLinesPresicion();
+            for (String s:
+                    matrixLines) {
+                myWriter.write(s);
+                myWriter.write("\n");
+            }
+            myWriter.write("\n");
+            myWriter.close();
+        } catch (Exception e){
+            System.out.println("Error in printing matrix to file!");
+        }
+    }
+
+    public static void PrintMatrix(MyMatrix matrix, String fileName){
+        try{
+            FileWriter myWriter = new FileWriter(fileName, true);
+            List<String> matrixLines = matrix.ToLines();
+            for (String s:
+                 matrixLines) {
+                myWriter.write(s);
+                myWriter.write("\n");
+            }
+            myWriter.write("\n");
+            myWriter.close();
+        } catch (Exception e){
+            System.out.println("Error in printing matrix to file!");
+        }
+    }
+
+    public static void PrintLine(String line, String fileName){
+        try{
+            FileWriter myWriter = new FileWriter(fileName, true);
+            myWriter.write(line);
+            myWriter.write("\n\n");
+            myWriter.close();
+        } catch (Exception e){
+            System.out.println("Error in printing line to file!");
+        }
+    }
+
     public static void main(String[] args) {
+        String fileName = "output.txt";
+        try{
+            FileWriter myWriter = new FileWriter(fileName);
+            myWriter.close();
+        } catch (Exception e){
+            System.out.println("Error in opening output file!");
+        }
         MyMatrix taskMatrix = new MyMatrix(GetMatrix());
+
+
         MatrixIterations(taskMatrix);
     }
 }
--- src/matrix/MyMatrix.java
+++ src/matrix/MyMatrix.java
@@ -1,6 +1,7 @@
 package matrix;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MyMatrix {
@@ -8,6 +9,17 @@ public class MyMatrix {
 
     public MyMatrix() {
         this.field = new ArrayList<>();
+    }
+
+    public MyMatrix(int n) {//generates square matrix
+        this.field = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            List<Double> line = new ArrayList<>();
+            for (int j = 0; j < n; j++) {
+                line.add(0.);
+            }
+            this.field.add(line);
+        }
     }
 
     public MyMatrix(List<List<Double>> nField) {
@@ -28,21 +40,29 @@ public class MyMatrix {
         return c;
     }
 
-    public MyMatrix Mult(MyMatrix a, MyMatrix b) {
-        int n = this.field.size();
+    public static MyMatrix Mult(MyMatrix a, MyMatrix b) {
+        int n = a.field.size();
+        int m = a.field.get(0).size();
+        int n2 = b.field.size();
+        int m2 = b.field.get(0).size();
+        MyMatrix c ;
+        if(m == n2){
         List<List<Double>> nField = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             List<Double> row = new ArrayList<>();
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m2; j++) {
                 Double sum = 0.;
-                for (int k = 0; k < n; k++) {
-                    sum += a.field.get(i).get(k) * a.field.get(k).get(j);
+                for (int k = 0; k < m; k++) {
+                    sum += a.field.get(i).get(k) * b.field.get(k).get(j);
                 }
                 row.add(sum);
             }
             nField.add(row);
         }
-        MyMatrix c = new MyMatrix(nField);
+        c = new MyMatrix(nField);
+        } else {
+            c = new MyMatrix();
+        }
         return c;
     }
 
@@ -58,5 +78,112 @@ public class MyMatrix {
         }
         MyMatrix tMatrix = new MyMatrix(tField);
         return tMatrix;
+    }
+
+    public static Double det(MyMatrix matrix){
+        if(matrix.field.size() > 1){
+            Double sum = 0.;
+            for(int i = 0; i < matrix.field.size(); i++){
+                MyMatrix smallMatrix = new MyMatrix();
+                for (int j = 1; j < matrix.field.size(); j++) {
+                    List<Double> line = new ArrayList<>();
+                    for (int k = 0; k < matrix.field.size(); k++) {
+                        if(k != i) {
+                            line.add(matrix.field.get(j).get(k));
+                        }
+                    }
+                    smallMatrix.field.add(line);
+                }
+                double sign = (i % 2 == 0) ? 1 : -1;
+                sum += matrix.field.get(0).get(i) * sign * det(smallMatrix);
+            }
+            return sum;
+        } else if(matrix.field.size() == 1)  {
+            return matrix.field.get(0).get(0);
+        } else {
+            return 1.;
+        }
+    }
+
+    public MyMatrix MinusOnePower(){
+        MyMatrix matrix = new MyMatrix();
+        Double detMatrix = MyMatrix.det(this);
+        for (int i = 0; i < this.field.size(); i++) {
+            List<Double> line = new ArrayList<>();
+            for (int j = 0; j < this.field.size(); j++) {
+                MyMatrix smallMatrix = new MyMatrix();
+                for (int k = 0; k < this.field.size(); k++) {
+                    if(k != i) {
+                        List<Double> sLine = new ArrayList<>();
+                        for (int l = 0; l < this.field.size(); l++) {
+                            if (l != j){
+                                sLine.add(this.field.get(k).get(l));
+                            }
+                        }
+                        smallMatrix.field.add(sLine);
+                    }
+                }
+                int sign = (i+j) % 2 == 0 ? 1 : -1;
+                line.add(sign * MyMatrix.det(smallMatrix)/detMatrix);
+            }
+            matrix.field.add(line);
+        }
+        return matrix.T();
+    }
+
+    public void SwapLines(int n1, int n2) {
+        int n = this.field.size();
+        for (int i = 0; i < n; i++) {
+            Double tmp = this.field.get(n1).get(i);
+            this.field.get(n1).set(i, this.field.get(n2).get(i));
+            this.field.get(n2).set(i, tmp);
+        }
+    }
+
+    public String ToLine(){
+        StringBuilder line = new StringBuilder();
+        for (int i = 0; i < this.field.size(); i++) {
+            for (int j = 0; j < this.field.get(0).size(); j++) {
+                line.append(String.format("%11.7f ", this.field.get(i).get(j)));
+            }
+        }
+        return line.toString();
+    }
+
+    public List<String> ToLines(){
+        List<String> lines = new ArrayList<>();
+        for (int i = 0; i < this.field.size(); i++) {
+            StringBuilder line = new StringBuilder();
+            for (int j = 0; j < this.field.get(0).size(); j++) {
+                line.append(String.format("%11.7f ", this.field.get(i).get(j)));
+            }
+            lines.add(line.toString());
+        }
+        return lines;
+    }
+
+    public List<String> ToLinesPresicion(){
+        List<String> lines = new ArrayList<>();
+        for (int i = 0; i < this.field.size(); i++) {
+            StringBuilder line = new StringBuilder();
+            for (int j = 0; j < this.field.get(0).size(); j++) {
+                line.append(String.format("%22.13e ", this.field.get(i).get(j)));
+            }
+            lines.add(line.toString());
+        }
+        return lines;
+    }
+
+    public static MyMatrix Copy(MyMatrix matrix){
+        List<List<Double>> newField = new ArrayList<>();
+        for (int i = 0; i < matrix.field.size(); i++) {
+            List<Double> line = new ArrayList<>();
+            for (int j = 0; j < matrix.field.get(0).size(); j++) {
+                line.add(matrix.field.get(i).get(j));
+            }
+            newField.add(line);
+        }
+        MyMatrix newMatrix = new MyMatrix(newField);
+        return newMatrix;
     }
 }
